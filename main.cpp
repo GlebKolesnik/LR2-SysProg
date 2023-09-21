2cd648cb6a53f85075138688af77805d91cbfc7b
Hlib <105455865+GlebKolesnik@users.noreply.github.com>
2023-09-21 13:19:33 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,56 +6,56 @@
 
 using namespace std;
 
-// Структура для представления конечного автомата
+// Structure for representing a state machine
 struct FiniteAutomaton {
-    set<int> states;  // Множество состояний
-    int initial_state; // Исходное состояние
-    set<int> final_states; // Множество финальных состояний
-    map<pair<int, char>, int> transitions; // Функция переходов (состояние, символ) -> следующее состояние
+    set<int> states;  // set states
+    int initial_state; // initial state
+    set<int> final_states; // set final states
+    map<pair<int, char>, int> transitions; // Transition function (state, symbol) -> next state
 };
 
-// Функция для проверки, допускает ли автомат слово w
+// Function to check if the machine accepts the word w
 bool acceptsWord(const FiniteAutomaton& automaton, const string& w) {
     int current_state = automaton.initial_state;
 
-    // Обработка w
+    // Process w
     for (char c : w) {
         auto transition = automaton.transitions.find({ current_state, c });
         if (transition == automaton.transitions.end()) {
-            return false; // Нет перехода для символа c
+            return false; // No transition for character c
         }
         current_state = transition->second;
     }
 
-    return automaton.final_states.count(current_state) > 0; // Проверка финального состояния
+    return automaton.final_states.count(current_state) > 0; // Checking the final state
 }
 
 int main() {
     setlocale(LC_ALL, "Russian");
     FiniteAutomaton automaton;
 
-    // Здесь задайте ваше фиксированное слово "w0"
+    // Fixed word "w0" here
     string w0 = "moloko";
 
-    // Считывание данных о конечном автомате из файла
+    // Read data about the state machine from a file
     ifstream input("automaton.txt");
     if (!input.is_open()) {
-        cout << "Не удалось открыть файл." << endl;
+        cout << "РќРµ СѓРґР°Р»РѕСЃСЊ РѕС‚РєСЂС‹С‚СЊ С„Р°Р№Р»." << endl;
         return 1;
     }
 
-    // Считывание ||A||
+    // Read ||A|| alphabet
     int alphabet_size;
     input >> alphabet_size;
 
-    // Считывание ||S||
+    // Read ||S||
     int num_states;
     input >> num_states;
 
-    // Считывание исходного состояния s0
+    // Read the initial state s0
     input >> automaton.initial_state;
 
-    // Считывание ||F|| и финальных состояний
+    // Read ||F|| and final states
     int num_final_states;
     input >> num_final_states;
     for (int i = 0; i < num_final_states; i++) {
@@ -64,7 +64,7 @@ int main() {
         automaton.final_states.insert(final_state);
     }
 
-    // Считывание функции переходов
+    // Read the transition function
     int s, s_next;
     char a;
     while (input >> s >> a >> s_next) {
@@ -73,20 +73,20 @@ int main() {
 
     input.close();
 
-    // Запрос у пользователя ввода слова "w1"
-    cout << "Введите слово w1: ";
+    // Prompt the user to enter the word "w1"
+    cout << "Р’РІРµРґРёС‚Рµ СЃР»РѕРІРѕ w1: ";
     string w1;
     cin >> w1;
 
-    // Проверка слова "w0 + w1"
+    // Checking the word "w0 + w1"
     string w = w0 + w1;
     bool result = acceptsWord(automaton, w);
 
     if (result) {
-        cout << "Конечный автомат допускает слово w = " << w << endl;
+        cout << "РљРѕРЅРµС‡РЅС‹Р№ Р°РІС‚РѕРјР°С‚ РґРѕРїСѓСЃРєР°РµС‚ СЃР»РѕРІРѕ w = " << w << endl;
     }
     else {
-        cout << "Конечный автомат не допускает слово w = " << w << endl;
+        cout << "РљРѕРЅРµС‡РЅС‹Р№ Р°РІС‚РѕРјР°С‚ РЅРµ РґРѕРїСѓСЃРєР°РµС‚ СЃР»РѕРІРѕ w = " << w << endl;
     }
 
     return 0;
